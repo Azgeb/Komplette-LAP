03b196b762b052883f35e2d4ec63eefbfa884c92
Azgeb <joseph.juri@gmail.com>
2020-02-10 22:32:56 +0100

    Changed secret area

--- sites/secret.php
+++ sites/secret.php
@@ -18,7 +18,15 @@ $events = $database->getEvents();
 <!DOCTYPE html>
 <html lang="en">
 <title>Website</title>
-<?php allHeadEntrys() ?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="/../src/css/styles.css">
+<link rel="stylesheet" href="/../src/css/navbar.css">
+<link rel="stylesheet" href="/../src/css/event.css">
 
 <body>
     <!-- Navbar -->
@@ -26,9 +34,9 @@ $events = $database->getEvents();
         <ul style="display: flex;">
             <li> <a href="../index.html" class="bar-item button padding-large white">Home</a></li>
             <div style="flex-grow: 1;"></div>
-            <?php if($user->userRole == 0) echo'<li><a href="admin.php" class="bar-item button padding-large white">Admin Area</a></li>'?>
+            <?php if ($user->userRole == 0) echo '<li><a href="admin.php" class="bar-item button padding-large white">Admin Area</a></li>' ?>
             <li><a href="logout.php" class="bar-item button padding-large white">Logout</a></li>
-           
+
     </div>
     <!-- Content -->
     <div style=" display: block;margin-left: auto;margin-right: auto;width: max-content;">
@@ -36,15 +44,15 @@ $events = $database->getEvents();
             <h1>Offene Termine</h1>
             <?php
             foreach ($events as &$event) {
-               drawEvent($event,$database, $user);
+                drawEvent($event, $database, $user);
             }
             ?>
         </form>
     </div>
     <!-- Footer -->
-    <footer class="container padding-64 center opacity">
-        <div class="xlarge padding-32">
-            <i class="fa fa-github"></i>
+    <footer>
+        <div>
+            <p>LAP (c) 2020</p>
         </div>
     </footer>
 </body>
