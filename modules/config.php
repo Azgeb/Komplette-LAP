329e5d9f08da72ce373d1e96cdcd5563f13b60c5
Azgeb <joseph.juri@gmail.com>
2020-02-10 22:35:36 +0100

    Updated config

--- modules/config.php
+++ modules/config.php
@@ -5,9 +5,9 @@ require_once(__DIR__.'/functions.php');
 require_once(__DIR__.'/../classes/event.php');
 require_once(__DIR__.'/../classes/user.php');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'test');
-define('DB_NAME', 'LAP');
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
 
 $database = new Database(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD);
