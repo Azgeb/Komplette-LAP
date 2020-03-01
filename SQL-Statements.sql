f5851a1f1ec88f4e1fd37f85424de8e5259f95f2
Azgeb <joseph.juri@gmail.com>
2020-03-01 19:58:55 +0100

    Updated SQL-Statements
    - Updated the Database creation statements to math the LAP criteria

--- SQL-Statements.sql
+++ SQL-Statements.sql
@@ -1,48 +1,123 @@
-CREATE DATABASE LAP;
+CREATE DATABASE Onlinekursverwaltung;
 
-USE LAP;
+USE Onlinekursverwaltung;
 
-CREATE TABLE `t_user` ( 
-  `id` INT NOT NULL AUTO_INCREMENT ,
+CREATE TABLE `t_internal_user` ( 
+  `internal_user_id` INT NOT NULL AUTO_INCREMENT ,
+  `is_administrator` VARCHAR(1) NOT NULL ,
   `email` VARCHAR(255) NOT NULL ,
   `password` VARCHAR(255) NOT NULL ,
   `firstname` VARCHAR(255) NOT NULL DEFAULT '' ,
   `lastname` VARCHAR(255) NOT NULL DEFAULT '' ,
-  `user_role` INT NOT NULL DEFAULT 1,
+  `postal_code` VARCHAR(255) NOT NULL ,
+  `city` VARCHAR(255) NOT NULL ,
+  `street` VARCHAR(255) NOT NULL DEFAULT '' ,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
   `updated_at` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-  PRIMARY KEY (`id`), UNIQUE (`email`)
+  PRIMARY KEY (`internal_user_id`), UNIQUE (`email`)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `t_course` ( 
+  `course_id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(255) NOT NULL ,
+  `internal_user_id` INT NOT NULL,
+  PRIMARY KEY (`course_id`)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `t_user` ( 
+  `user_id` INT NOT NULL AUTO_INCREMENT ,
+  `course_id` INT NOT NULL,
+  `email` VARCHAR(255) NOT NULL ,
+  `password` VARCHAR(255) NOT NULL ,
+  `firstname` VARCHAR(255) NOT NULL DEFAULT '' ,
+  `lastname` VARCHAR(255) NOT NULL DEFAULT '' ,
+  `postal_code` VARCHAR(255) NOT NULL ,
+  `city` VARCHAR(255) NOT NULL ,
+  `street` VARCHAR(255) NOT NULL DEFAULT '' ,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `updated_at` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  PRIMARY KEY (`user_id`), UNIQUE (`email`)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `t_document` ( 
+  `document_id` INT NOT NULL AUTO_INCREMENT ,
+  `course_id` VARCHAR(255) NOT NULL ,
+  `path` VARCHAR(255) NOT NULL ,
+  `display_name` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`document_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO t_user
-(email, password, firstname, lastname, user_role)
+(course_id, email, password, firstname, lastname, postal_code, city, street)
 values
-('example@example.com','$2y$10$qCgb4MKzbMKAqUU2LOFBQ.wGoAD6yBElFA7V7EPwK.QGCViJjx4mu','Test','Example',0);
+(1,'example@user.com','$2y$10$qCgb4MKzbMKAqUU2LOFBQ.wGoAD6yBElFA7V7EPwK.QGCViJjx4mu','Test','Example','0000', 'city', 'street');
 
-CREATE TABLE `t_event` ( 
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `heading` VARCHAR(255) NOT NULL ,
-  `description` VARCHAR(255) NOT NULL ,
-  `event_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-  PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-INSERT INTO t_event
-(heading, description)
+INSERT INTO t_internal_user
+(internal_user_id,is_administrator, email, password, firstname, lastname, postal_code, city, street)
 values
-('LAP','This is an default description for the LAP!!');
+(1,'y','admin@intern.com','$2y$10$qCgb4MKzbMKAqUU2LOFBQ.wGoAD6yBElFA7V7EPwK.QGCViJjx4mu','Admin','Intern','0000', 'city', 'street');
 
-INSERT INTO t_event
-(heading, description)
+INSERT INTO t_internal_user
+(internal_user_id, is_administrator, email, password, firstname, lastname, postal_code, city, street)
 values
-('!LAP!','!!!!!!!!!!!!!!!!!!!!!!!!!This is an default description for the LAP!!');
+(2,'n','trainer@intern.com','$2y$10$qCgb4MKzbMKAqUU2LOFBQ.wGoAD6yBElFA7V7EPwK.QGCViJjx4mu','Trainer','Intern','0000', 'city', 'street');
 
-CREATE TABLE t_event_user
-( user_id INT(11) NOT NULL COMMENT 'PK, FK ref t_user(id)'
-, event_id INT(11) NOT NULL COMMENT 'PK, FK ref t_event(id)'
-, PRIMARY KEY (user_id, event_id)
-);
+INSERT INTO t_course
+(course_id, name, internal_user_id)
+values
+(1, 'Informatik', 1);
 
-INSERT INTO t_event_user(user_id, event_id)
-values(1,1);
+INSERT INTO t_course
+(course_id, name, internal_user_id)
+values
+(2, 'Mechatronik', 1);
+
+INSERT INTO t_course
+(course_id, name, internal_user_id)
+values
+(3, 'Technik', 1);
+
+INSERT INTO t_document
+(course_id, path, display_name)
+values
+(1, 'assets/Informatik1.pdf', 'Informatik 1');
+
+INSERT INTO t_document
+(course_id, path, display_name)
+values
+(1, 'assets/Informatik2.pdf', 'Informatik 2');
+
+INSERT INTO t_document
+(course_id, path, display_name)
+values
+(1, 'assets/Informatik3.pdf', 'Informatik 3');
+
+INSERT INTO t_document
+(course_id, path, display_name)
+values
+(2, 'assets/Mechatronik1.pdf', 'Mechatronik 1');
+
+INSERT INTO t_document
+(course_id, path, display_name)
+values
+(2, 'assets/Mechatronik2.pdf', 'Mechatronik 2');
+
+INSERT INTO t_document
+(course_id, path, display_name)
+values
+(2, 'assets/Mechatronik3.pdf', 'Mechatronik 3');
+
+INSERT INTO t_document
+(course_id, path, display_name)
+values
+(3, 'assets/Technik1.pdf', 'Technik 1');
+
+INSERT INTO t_document
+(course_id, path, display_name)
+values
+(3, 'assets/Technik2.pdf', 'Technik 2');
+
+INSERT INTO t_document
+(course_id, path, display_name)
+values
+(3, 'assets/Technik3.pdf', 'Technik 2');
