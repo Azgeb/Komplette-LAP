607e944e8e6136f45656ad560e1ab79cecce5482
Azgeb <joseph.juri@gmail.com>
2020-03-01 20:02:34 +0100

    Updated secret.php
    - updated to match LAP requirements

--- sites/secret.php
+++ sites/secret.php
@@ -16,22 +16,14 @@ if (!$user || !$user->email) {
     // Asks the user to login if the secret.php got accesed via the searchbar 
     die('Bitte zuerst <a href="login.php">einloggen</a>');
 }
-// Reads all events from the database 
-$events = $database->getEvents();
+// Reads all courses from the database 
+$course = $database->getCourse($user->courseId);
+$documents = $database->getDocuments($course->courseId);
 
-// Checks if the joinEvent form is submited
-if (isset($_GET['joinEvent'])) {
-    // sets the submited eventId and joins the event as the logged in user
-    $eventId = $_POST['eventId'];
-    $result = $database->joinEvent($eventId, $user->id);
-    //Checks the response from the database operation
-    if ($result && $result !== false) {
-        //header("Location: /sites/admin.php", true, 301);
-    } else {
-        // Sets the errorMessage variable 
-        $errorMessage = "Ein Fehler ist aufgetreten.<br>";
-    }
-}
+echo '<script>';
+echo 'console.log(' . json_encode($documents) . ')';
+echo '</script>';
+
 ?>
 
 <!DOCTYPE html>
@@ -62,27 +54,25 @@ if (isset($_GET['joinEvent'])) {
     }
     ?>
     <div style=" display: block;margin-left: auto;margin-right: auto;width: max-content;">
-        <h1>Offene Termine</h1>
-        <?php
-        // creates one entry in the html for each object in the events array
-        foreach ($events as &$event) {
-            // Checks if the user alredy joined an event
-            $alreadyJoined = $database->isUserJoinedEvent($event->id, $user->id); ?>
-            <div class="event-area <?php
-                                    // Sets a HTML class depending on the alreadyJoined variable
-                                    if ($alreadyJoined) echo 'event-joined'
-                                    ?>">
-                <h2 class="event-heading"> <?php echo $event->heading ?> </h2>
-                <h3> <?php echo $event->description ?> </h3>
-                <p> Die Veranstaltung findet am <?php echo $event->eventDate->format('d.m.Y') ?> statt.</p>
-                <form action="?joinEvent=1" method="post">
-                    <input type="text" name="eventId" value="<?php echo $event->id ?>" hidden="true">
-                    <?php if (!$alreadyJoined) echo '<input type="submit" value="Teilnehem">' ?>
-                </form>
-            </div>
-        <?php
-        }
-        ?>
+        <h1>Guten Tag,  <?php echo $user->firstname, ' ', $user->lastname ?> </h1>
+        <h3>Ihre Dokumente für den Kurs <?php echo $course->name ?> </h3>
+        <table style="width:100%">
+            <tr style="text-align: left;">
+                <th>Dateiname</th>
+                <th>Datei</th>
+            </tr>
+            <?php
+            // creates one entry in the html for each object in the courses array
+            foreach ($documents as &$document) {
+            ?>
+                <tr>
+                    <td><?php echo $document->displayName ?></td>
+                    <td><a href="../<?php echo $document->path ?>">download</a></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
     </div>
     <!-- Footer -->
     <footer>
