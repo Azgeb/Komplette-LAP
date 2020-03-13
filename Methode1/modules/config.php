c1294f50b50cccf4030835c809230932e0e84f66
Azgeb <joseph.juri@gmail.com>
2020-03-13 23:11:24 +0100

    Updated modules

--- Methode1/modules/config.php
+++ Methode1/modules/config.php
@@ -5,10 +5,10 @@ require_once('../classes/internalUser.php');
 require_once('../classes/user.php');
 require_once('../classes/course.php');
 
-define('DB_HOST', '');
-define('DB_USER', '');
-define('DB_PASSWORD', '');
-define('DB_NAME', '');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'test');
+define('DB_NAME', 'onlinekursverwaltung');
 
 $database = new Database(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD);
 
