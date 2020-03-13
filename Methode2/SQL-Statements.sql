1c49508959178498069ba855c39bd152d634ee81
Azgeb <joseph.juri@gmail.com>
2020-03-13 20:53:45 +0100

    Added Methode 2

---
+++ Methode2/SQL-Statements.sql
@@ -0,0 +1,209 @@
+/*
+- ADMINISTRATOR : admin@intern.com    / test  
+- TRAINER       : trainer@intern.com  / test  
+- USER          : example@user.com    / test  
+*/
+
+CREATE DATABASE onlinekursverwaltung; 
+
+USE onlinekursverwaltung; 
+
+
+CREATE TABLE `t_course` 
+  ( 
+     `course_id`        INT NOT NULL auto_increment, 
+     `name`             VARCHAR(255) NOT NULL, 
+     `user_id` INT NOT NULL, 
+     PRIMARY KEY (`course_id`) 
+  ) 
+engine = innodb 
+DEFAULT charset=utf8 
+COLLATE=utf8_unicode_ci; 
+
+CREATE TABLE `t_user` 
+  ( 
+     `user_id`     INT NOT NULL auto_increment, 
+     `course_id`   INT NOT NULL, 
+     `email`       VARCHAR(255) NOT NULL, 
+     `password`    VARCHAR(255) NOT NULL, 
+     `firstname`   VARCHAR(255) NOT NULL DEFAULT '', 
+     `lastname`    VARCHAR(255) NOT NULL DEFAULT '', 
+     `postal_code` VARCHAR(255) NOT NULL, 
+     `city`        VARCHAR(255) NOT NULL, 
+     `street`      VARCHAR(255) NOT NULL DEFAULT '', 
+     `created_at`  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     `user_role`   INT NOT NULL, 
+     PRIMARY KEY (`user_id`), 
+     UNIQUE (`email`) 
+  ) 
+engine = innodb 
+DEFAULT charset=utf8 
+COLLATE=utf8_unicode_ci; 
+
+CREATE TABLE `t_document` 
+  ( 
+     `document_id`  INT NOT NULL auto_increment, 
+     `course_id`    VARCHAR(255) NOT NULL, 
+     `path`         VARCHAR(255) NOT NULL, 
+     `display_name` VARCHAR(255) NOT NULL, 
+     PRIMARY KEY (`document_id`) 
+  ) 
+engine = innodb 
+DEFAULT charset=utf8 
+COLLATE=utf8_unicode_ci; 
+
+INSERT INTO t_user 
+            (course_id, 
+             email, 
+             password, 
+             firstname, 
+             lastname, 
+             postal_code, 
+             city, 
+             street,
+             user_role) 
+VALUES      (1, 
+             'example@user.com', 
+             '$2y$10$qCgb4MKzbMKAqUU2LOFBQ.wGoAD6yBElFA7V7EPwK.QGCViJjx4mu', 
+             'Test', 
+             'Example', 
+             '0000', 
+             'city', 
+             'street',
+             2); 
+
+INSERT INTO t_user 
+            (course_id, 
+             email, 
+             password, 
+             firstname, 
+             lastname, 
+             postal_code, 
+             city, 
+             street,
+             user_role) 
+VALUES      (0, 
+             'admin@intern.com', 
+             '$2y$10$qCgb4MKzbMKAqUU2LOFBQ.wGoAD6yBElFA7V7EPwK.QGCViJjx4mu', 
+             'Admin', 
+             'Intern', 
+             '0000', 
+             'city', 
+             'street',
+             0); 
+
+INSERT INTO t_user 
+            (course_id, 
+             email, 
+             password, 
+             firstname, 
+             lastname, 
+             postal_code, 
+             city, 
+             street,
+             user_role) 
+VALUES      (0, 
+             'trainer@intern.com', 
+             '$2y$10$qCgb4MKzbMKAqUU2LOFBQ.wGoAD6yBElFA7V7EPwK.QGCViJjx4mu', 
+             'Trainer', 
+             'Intern', 
+             '0000', 
+             'city', 
+             'street',
+             1); 
+
+INSERT INTO t_course 
+            (course_id, 
+             name, 
+             user_id) 
+VALUES      (1, 
+             'Informatik', 
+             1); 
+
+INSERT INTO t_course 
+            (course_id, 
+             name, 
+             user_id) 
+VALUES      (2, 
+             'Mechatronik', 
+             1); 
+
+INSERT INTO t_course 
+            (course_id, 
+             name, 
+             user_id) 
+VALUES      (3, 
+             'Technik', 
+             1); 
+
+INSERT INTO t_document 
+            (course_id, 
+             path, 
+             display_name) 
+VALUES      (1, 
+             'src/assets/Informatik1.pdf', 
+             'Informatik 1'); 
+
+INSERT INTO t_document 
+            (course_id, 
+             path, 
+             display_name) 
+VALUES      (1, 
+             'src/assets/Informatik2.pdf', 
+             'Informatik 2'); 
+
+INSERT INTO t_document 
+            (course_id, 
+             path, 
+             display_name) 
+VALUES      (1, 
+             'src/assets/Informatik3.pdf', 
+             'Informatik 3'); 
+
+INSERT INTO t_document 
+            (course_id, 
+             path, 
+             display_name) 
+VALUES      (2, 
+             'src/assets/Mechatronik1.pdf', 
+             'Mechatronik 1'); 
+
+INSERT INTO t_document 
+            (course_id, 
+             path, 
+             display_name) 
+VALUES      (2, 
+             'src/assets/Mechatronik2.pdf', 
+             'Mechatronik 2'); 
+
+INSERT INTO t_document 
+            (course_id, 
+             path, 
+             display_name) 
+VALUES      (2, 
+             'src/assets/Mechatronik3.pdf', 
+             'Mechatronik 3'); 
+
+INSERT INTO t_document 
+            (course_id, 
+             path, 
+             display_name) 
+VALUES      (3, 
+             'src/assets/Technik1.pdf', 
+             'Technik 1'); 
+
+INSERT INTO t_document 
+            (course_id, 
+             path, 
+             display_name) 
+VALUES      (3, 
+             'src/assets/Technik2.pdf', 
+             'Technik 2'); 
+
+INSERT INTO t_document 
+            (course_id, 
+             path, 
+             display_name) 
+VALUES      (3, 
+             'src/assets/Technik3.pdf', 
+             'Technik 2'); 
