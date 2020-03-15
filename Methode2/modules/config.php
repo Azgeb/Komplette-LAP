516fb0832ec30db931400aafb9e4ec1855966e0c
Azgeb <joseph.juri@gmail.com>
2020-03-15 19:37:16 +0100

    Added modules of Methode2

---
+++ Methode2/modules/config.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('../classes/database.php');
+require_once('../classes/user.php');
+require_once('../classes/course.php');
+
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+
+$database = new Database(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD);
+
+?>
